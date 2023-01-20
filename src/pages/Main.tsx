526e911cc361b96a6467fb21f39bb62aeb8337e7
Demog0rg0n <ivan.kot1331@gmail.com>
2023-01-20 21:26:36 +0300

    тест

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 
-import CatalogLink from '../components/mainPage/CatalogLink'
-
 import RecommendationsSwiper from '../components/mainPage/RecomSwiper/RecommendationsSwiper'
 import Top10Swipers from '../components/mainPage/top10swipers'
+import CatalogLink from '../components/mainPage/CatalogLink'
 
 import '../styles/main.scss'
 
